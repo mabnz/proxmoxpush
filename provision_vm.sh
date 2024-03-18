f6e1592916488be7c18e1c2ed6d9ab5a8254353e
mabnz <mike@f9.nz>
2024-03-19 08:58:47 +1300

    Fixed errors

--- provision_vm.sh
+++ provision_vm.sh
@@ -4,18 +4,19 @@
 ### Don't touch these
 ############################
 
-FILE=$1
+FILEPATH=$1
+FILENAME=`basename $1`
 UNIXTIME=`date +%s`
 VMNAME=$2
-diskcount=0
+DISKCOUNT=0
 
 ### Modify these variables to suit
 ############################
 
-STORAGE="local-lvm"
-HOST="proxm1"
-USER="root"
-VLAN_TAG=205
+STORAGE="storage"
+HOST="proxmox"
+USER="username"
+VLAN_TAG=6000
 
 
 
@@ -23,29 +24,30 @@ VLAN_TAG=205
 ### Script start
 ############################
 
-if [[ ! $1 ]]; then echo -e "ERROR: No -kvm.zip filename specified. Example usage: $0 FGT_VM64_KVM-v7.4.3.F.out.kvm.zip <VM name (no spaces)>\n\n" && exit; fi 
-if [[ ! $2 ]]; then echo -e "ERROR: No -kvm.zip filename specified. Exanple usage: $0 FGT_VM64_KVM-v7.4.3.F.out.kvm.zip <VM name (no spaces)>\n\n" && exit; fi 
-
+if [[ ! $1 ]]; then echo -e "ERROR: No filename specified. Example: $0 filename.kvm.zip <VM name (no spaces)>\n\n" && exit; fi 
+if [[ ! $2 ]]; then echo -e "\nERROR: Specify VM name. Example: $0 filename.zip <VM name (no spaces)>\n" && exit; fi 
+if [ `echo $2 | grep '[-_ ]'` ]; then echo -e "\nERROR: Only alphanumeric chars permitted in VM name\n" && exit; fi 
 
 NEXT_ID=`ssh $HOST pvesh get /cluster/nextid`
 echo "--- Transferring firmware to proxmox..."
-scp -q $1 $USER@$HOST:/tmp/$1
+scp $1 $USER@$HOST:/tmp/
 
 echo "--- Unzipping firmware..."
-ssh $HOST "cd /tmp && mkdir _tmp_$UNIXTIME && unzip -qq /tmp/$1 -d /tmp/_tmp_$UNIXTIME"
+ssh $HOST "cd /tmp && mkdir _tmp_$UNIXTIME && unzip -qq /tmp/$FILENAME -d /tmp/_tmp_$UNIXTIME"
 
 # Create VM
 echo "--- Creating $VMNAME ($NEXT_ID) with 4C/8GB..."
 ssh $HOST "echo $VMNAME >/tmp/vmname"
-ssh $HOST qm create $NEXT_ID --memory 8192 --balloon 4096 --cores 4 --name '`cat /tmp/vmname`'  --net0 model=virtio,bridge=vmbr0,tag=$VLAN_TAG --ostype l26 --autostart 0
+ssh $HOST "qm create $NEXT_ID --memory 8192 --balloon 4096 --cores 4 --scsihw=virtio-scsi-pci --pool 'Provisioned_VMs' --name '$VMNAME'  --net0 model=virtio,bridge=vmbr0,tag=$VLAN_TAG --ostype l26 --autostart 0"
 
 # Create file list, import & link disk to VM
-unzip -l $1 | grep qcow | awk '{print $4}' | while read line; do {
+items=`unzip -l $1 | grep qcow | awk '{print $4}' | sort`
+for item in $items; do {
 	
-	echo "--- Importing disk image $line.."
-	ssh $HOST "qm disk import $NEXT_ID /tmp/_tmp_$UNIXTIME/$line $STORAGE"
-	ssh $HOST "qm set $NEXT_ID -scsi$diskcount file=$STORAGE:vm-$NEXT_ID-disk-$diskcount >/dev/null"
-	((diskcount++))
+	echo "--- Importing disk image $item.."
+	ssh $HOST "qm disk import $NEXT_ID /tmp/_tmp_$UNIXTIME/$item $STORAGE >/dev/null" >/dev/null
+	ssh $HOST "qm set $NEXT_ID -scsi$DISKCOUNT file=$STORAGE:vm-$NEXT_ID-disk-$DISKCOUNT" >/dev/null
+	DISKCOUNT=$((DISKCOUNT+1))
 }; done
 
 # Correct boot order
@@ -53,6 +55,6 @@ ssh $HOST "qm set $NEXT_ID -boot order=scsi0 >/dev/null"
 
 
 # Cleanup
-ssh $HOST rm /tmp/$1
+ssh $HOST rm /tmp/$FILENAME
 ssh $HOST rm /tmp/vmname
 ssh $HOST rm -fr /tmp/_tmp_$UNIXTIME
